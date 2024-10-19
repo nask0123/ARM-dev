a911a0fe00f96044e844c450042cff2eae4e554a
ALHNk <88340494+ALHNk@users.noreply.github.com>
2024-10-19 16:54:05 +0500

    PersonNPC

--- Assets/Scripts/personNPC.cs
+++ Assets/Scripts/personNPC.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class personNPC : MonoBehaviour
 {
 	public float speed;
 	private Rigidbody2D rb;
 	public int moveX, moveY;
-	public LayerMask sideWalkLayer;
+	public LayerMask sideWalkLayer, playerLayer;
+	public GameObject dialogBox;
+	private string[] whatToSay;
 	
     // Start is called before the first frame update
     void Start()
-    {
+	{
+		whatToSay = new string[3];
+		whatToSay[0] = "Қарағым-ау не істеп жүрсің!";
+		whatToSay[1] = "Тамақ жегін к-келеді ме?";
+		whatToSay[2] = "ЭЭй кет әрі!";
+		
 	    rb = GetComponent<Rigidbody2D>();
+		dialogBox.SetActive(false);
     }
     
 
@@ -20,6 +29,7 @@ public class personNPC : MonoBehaviour
     void Update()
     {
 	    move();
+	    findPlayer();
     }
     
 	public void move(){
@@ -33,31 +43,61 @@ public class personNPC : MonoBehaviour
 			
 		}
 		if(other.tag == "crossWalk"){
-			Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 2f, sideWalkLayer);
-			changeDirection(1);
+			crossRoadConfig config = other.GetComponent<crossRoadConfig>();
+			changeDirection(config);
 		}
 	}
 	
-	public void changeDirection(int direction){
-		if (direction == 1) 
+	public void changeDirection(crossRoadConfig crc){
+		
+		List<int> possibleDirections = new List<int>();
+		
+		if (crc.Right) possibleDirections.Add(1);  
+		if (crc.Left) possibleDirections.Add(-1);  
+		if (crc.Up) possibleDirections.Add(2);     
+		if (crc.Down) possibleDirections.Add(-2); 
+		
+		if (possibleDirections.Count > 0)
 		{
-			moveX = 1;
-			moveY = 0;
+			int direction = possibleDirections[Random.Range(0, possibleDirections.Count)];
+
+			if (direction == 1) 
+			{
+				moveX = 1;
+				moveY = 0;
+			}
+			else if (direction == -1) 
+			{
+				moveX = -1;
+				moveY = 0;
+			}
+			else if (direction == 2) 
+			{
+				moveX = 0;
+				moveY = 1;
+			}
+			else if (direction == -2) 
+			{
+				moveX = 0;
+				moveY = -1;
+			}
 		}
-		else if (direction == -1) 
-		{
-			moveX = -1;
-			moveY = 0;
+	}
+	
+	public void findPlayer(){
+		Collider2D playerCollider = Physics2D.OverlapCircle(transform.position, 2f, playerLayer);
+		if(playerCollider != null){
+			int i = Random.Range(0,3);
+			talkToPlayer(whatToSay[i]);
 		}
-		else if (direction == 2) 
-		{
-			moveX = 0;
-			moveY = 1;
-		}
-		else if (direction == -2) 
-		{
-			moveX = 0;
-			moveY = -1;
-		}
+	}
+	public void talkToPlayer(string textToSay){
+		dialogBox.GetComponent<Text>().text = textToSay;
+		dialogBox.SetActive(true);
+		StartCoroutine(stopTalking());
+	}
+	IEnumerator stopTalking(){
+		yield return new WaitForSeconds(3);
+		dialogBox.SetActive(false);
 	}
 }
